c030f7c84bafa058078409c7a46db6699fd6d938
ssab <qasw4554@gmail.com>
2019-10-02 11:44:31 +0900

    add fixture
    tmpfileを複数作成するfixtureを作成

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import json
 from pytest import fixture
 
-@fixture
+
+@fixture(scope='function')
 def create_jsonfile(tmpdir):
     data = {"full_text": "abc"}
     tmpfile = tmpdir.join('text.json')
@@ -12,3 +13,23 @@ def create_jsonfile(tmpdir):
         )
     yield str(tmpfile)
     tmpfile.remove()
+
+
+@fixture(params=data)
+def create_jsonfiles(tmpdir):
+    # 前処理のcreate_jsonfileを10回行ってリストを返したい
+    data = [({"full_text": "野球が好き"}, "test1.json"),
+            ({"full_text": "野球があまり好きじゃないし、嫌い"}, "test2.json")
+            ]
+    jsonfiles = list()
+    for d in data:
+        tmpfile = tmpdir.join(d[1])
+        with tmpfile.open('w') as f:
+            json.dump(
+                d[0], f, ensure_ascii=False, indent=4,
+                sort_keys=True, separators=(',', ': ')
+            )
+        jsonfiles.append(tmpfile)
+    yield jsonfiles
+    for f in jsonfiles:
+        f.remove()
--- tests/test_co_occurrence_network.py
+++ tests/test_co_occurrence_network.py
@@ -1,4 +1,4 @@
-import pytest
+from pytest import mark
 import networkx as nx
 from src.co_occurrence_network import (co_occurrence_network,
                                        _add_node_and_edge,
@@ -6,21 +6,7 @@ from src.co_occurrence_network import (co_occurrence_network,
                                        _create_graph_in_CON)
 
 
-def test_co_occurrence_network(create_jsonfile):
+def test_co_occurrence_network(create_jsonfiles):
     graph = nx.Graph()
-    graph = co_occurrence_network([create_jsonfile], graph)
-    assert graph.nodes() == expected
-
-
-def test_add_node_and_edge():
-    words = {}
-    expected = 1
-    graph = nx.Graph()
-    morphological_analysis._add_node_and_edge(words, graph)
-    assert graph.nodes["とある単語v"]["count"] == expected
-
-
-def test_calc_npmi():
-    graph = nx.Graph()
-    morphological_analysis._calc_npmi(graph)
+    graph = co_occurrence_network(create_jsonfiles, graph)
     assert graph.nodes() == expected
