eeeff47902d76a1833281bfa4f094895259ce0f8
ssab <qasw4554@gmail.com>
2019-11-25 13:26:06 +0900

    add

--- src/2-w2v/2-vector.py
+++ src/2-w2v/2-vector.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from gensim.models.word2vec import Word2Vec
+from typing import List
 from numpy import zeros, ndarray
 from numpy.linalg import norm
 from pandas import read_csv, DataFrame
@@ -10,29 +11,26 @@ def df_vector(csv_path: Path, model_path: Path) -> DataFrame:
     DataFrameのwakati_textを1行ずつ単語ベクトルの総和を求める
     総和をDataFrameに追加
     '''
-    # 一応tolist()使わなくてもいいか試してみること
-    df = read_csv(csv_path)['wakati_text'].dropna()
-    # .dropnaしたあとにi, 2などをすると、要素番号がずれてしまうので一致しない可能性がある。その対処
-    wakati_texts = (row.split(" ") for row in df)
     model = Word2Vec.load(model_path)
+    df = read_csv(csv_path)['wakati_text'].dropna()
+    wakati_texts = (row.split(" ") for row in df)
     for i, row in enumerate(wakati_texts):
         # .dropnaしたあとにi, 2などをすると、要素番号がずれてしまうので一致しない可能性がある。その対処
+        # 正規化したあと、dfに書き込むために処理が必要
         df.iat[i, 2] = _normalize(_vector_sum(row, model))
     return df
 
 
-def _vector_sum(row: list, model: Word2Vec) -> ndarray:
+def _vector_sum(row: List[str], model: Word2Vec) -> ndarray:
     '''
-    引数rowの要素１つずつをベクトルに変換し、rowのベクトルの総和を求める
-    row: 1ツイートを形態素解析した単語のリスト
-    model: Word2Vecモデル
+    引数rowの単語１つずつをベクトルに変換し、rowのベクトルの総和を求める
+    params row: 1ツイートを形態素解析した単語のリスト
+    params model: Word2Vecモデル
+    return: numpy.ndarray
     '''
     word_vecs = (model.wv[word] for word in row)
-    # これと下のコメントアウト、結果が同じなのか気になる。
-    # あと、np.sumの第1引数がiterableでない気がするので、動くか不明
     line_vec = zeros(word_vecs[0].shape, dtype=word_vecs[0].dtype)
     for word_vec in word_vecs:
-        # ベクトルの総和を求める。他にいい感じでかけそう
         line_vec = line_vec + word_vec
     return line_vec
 
@@ -43,15 +41,6 @@ def _normalize(vec: ndarray) -> ndarray:
     正規化で0と1の間の相対値が残る
     '''
     return vec / norm(vec)
-
-
-# def _all_normalize2(vec):
-    # from sklearn.preprocessing import StandardScaler
-#     '''
-#     標準化、これでもできないかな？
-#     一応、ベクトルデータ全てに対して標準化を行うので注意
-#     '''
-#     return StandardScaler().fit_transform(vec)
 
 
 if __name__ == "__main__":
