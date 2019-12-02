67b2c6dc3d53b7556c4ba8d903ddebf38118203e
ssab <qasw4554@gmail.com>
2019-12-02 19:31:39 +0900

    add

--- src/1-2-has_other_label/2-w2v/model_test.py
+++ src/1-2-has_other_label/2-w2v/model_test.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from gensim.models.word2vec import Word2Vec
 
 
-twitter_path = str(Path.home()) + "/py/research/twitter/"
-model_path = twitter_path + "trend-死刑求刑.model"
+model_path = '/Users/ssab/py/research/src/1-2-has_other_label/data/trend-死刑求刑-0-label-study.model'
 
 model = Word2Vec.load(model_path)
 # print(model.wv.index2entity)
-print(model.wv.most_similar(positive=['反対'], topn=1000))
+# print(model.wv.most_similar(positive=['反対'], topn=1000))
+# print(model.wv.most_similar_to_given('死刑', ['みる', '内容', '目的', '求刑']))
 # print(model.wv.similarity('', ''))
