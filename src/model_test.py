d45218a6c303ea3bf56006d55942d1d3079de6a2
ssab <qasw4554@gmail.com>
2019-12-29 22:35:21 +0900

    add

--- src/model_test.py
+++ src/model_test.py
@@ -5,7 +5,8 @@ from gensim.models.word2vec import Word2Vec
 model_path = '/Users/ssab/py/research/data/trend-グレタさん-new.model'
 
 model = Word2Vec.load(model_path)
-print(model.wv.index2entity)
-# print(model.wv.most_similar(positive=['反対'], topn=1000))
+# print(model.wv.index2entity)
+print(model.wv.most_similar(positive=['グレタ'], topn=100))
+# print(model.wv.most_similar(positive=['グレタ'], negative=['洗脳'], topn=10))
 # print(model.wv.most_similar_to_given('死刑', ['みる', '内容', '目的', '求刑']))
 # print(model.wv.similarity('', ''))
